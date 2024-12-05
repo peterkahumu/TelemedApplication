0dd68e3940ec03d13986cdf448a0900d96a83f36
Peter Muhumuki <muhumukip@gmail.com>
2024-12-05 12:21:29 +0300

    Restructuring the entire user interface. Success for Base.html

--- static/js/menuBar.js
+++
@@ -1,7 +0,0 @@
-
-const mobileMenu = document.getElementById('mobile-menu');
-const navList = document.getElementById('nav-list');
-
-mobileMenu.addEventListener('click', () => {
-    navList.classList.toggle('active');
-});

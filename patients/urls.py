22004c5402f90725284a2517a8e6fd01ca06a91d
Peter Muhumuki <muhumukip@gmail.com>
2024-12-14 19:56:03 +0300

    Added the page for the user to view the appointments they have made.

--- patients/urls.py
+++ patients/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 # create your urls here
 urlpatterns = [
-
+    path('appointments/', PatientAppointments.as_view(), name="patient_appointments"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, render
+from authentication.models import User, Doctor
+from appointments.models import Appointment
+from django.contrib import messages
+from django.views import View
 
 # create your views here
+class PatientAppointments(View):
+    def get(self, request):
+        user = request.user
+        appointments = Appointment.objects.filter(user = user)
+
+        context = {
+            "appointments": appointments
+        }
+
+        return render(request, "patients/appointments.html", context)
